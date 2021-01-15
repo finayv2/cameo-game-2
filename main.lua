bc94e03009a142ef818f7d184c5653feb64c907a
finayv2 <75693103+finayv2@users.noreply.github.com>
2021-01-14 20:08:55 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/finayv2/Ui-Lib/main/azure%20ui%20lib%20modified.lua", true))()
-
+if not getgenv().MTAPIMutex then loadstring(game:HttpGet("https://raw.githubusercontent.com/KikoTheDon/MT-Api-v2/main/__source/mt-api%20v2.lua", true))() end
+getgenv().AlreadyExecuted = false
 local LocalPlayerTab = Library:CreateTab("LocalPlayer", "LocalPlayer Tab", "Dark")
 
 
@@ -9,7 +10,7 @@ LocalPlayerTab:CreateToggle("No Stun & Cooldown", function(value)
         if shared.NoStun then
 			for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
 				if v:IsA("Folder") then
-					if v.Name == "PUNCHM2COOLDOWN" or v.Name == "Busy" or v.Name == "Superslow" or v.Name == "Comboing" or v.Name == "PunchCooldown" or v.Name == "CTag" or v.Name == "Blocking" or v.Name == "IgnisCooldown" or v.Name == "Helpless" or v.Name == "StepBlock" or v.Name == "Staggered" or v.Name == "Ragdolled" or v.Name == "Superamor" or v.Name == "NoSpeedUpdate" or v.Name == "Impaled" or v.Name == "manashield" or v.Name == "IFrames" or v.Name == "ANGELFEATHERCD" or v.Name == "LannisCD" or v.Name == "SnapCooldown" then
+					if v.Name == "PUNCHM2COOLDOWN" or v.Name == "Busy" or v.Name == "Superslow" or v.Name == "Comboing" or v.Name == "PunchCooldown" or v.Name == "CTag" or v.Name == "IgnisCooldown" or v.Name == "Helpless" or v.Name == "StepBlock" or v.Name == "Staggered" or v.Name == "Ragdolled" or v.Name == "Superamor" or v.Name == "NoSpeedUpdate" or v.Name == "Impaled" or v.Name == "manashield" or v.Name == "IFrames" or v.Name == "ANGELFEATHERCD" or v.Name == "LannisCD" or v.Name == "SnapCooldown" or v.Name == "BeserkCooldown" then
 						v:Remove()
 					end
 				end
@@ -28,7 +29,11 @@ LocalPlayerTab:CreateToggle("No Stun & Cooldown", function(value)
 					elseif v:FindFirstChild("FlameCooldown") then
 						v.FlameCooldown:Remove()
 					elseif v:FindFirstChild("ThunderCooldown") then
+					
 						v.ThunderCooldown:Remove()
+					elseif v:FindFirstChild("DFCooldown") then
+
+						v.DFCooldown:Remove()
 					end
 				end
 			end		
@@ -36,16 +41,29 @@ LocalPlayerTab:CreateToggle("No Stun & Cooldown", function(value)
     end
 end)
 
+
+LocalPlayerTab:CreateToggle("No Cooldown Dodge", function(PogValue) 
+	shared.PogNoVCool = PogValue
+end)
+
+if getgenv().AlreadyExecuted == false then
+	ogWait = hookfunction(wait, function(x)
+		if shared.PogNoVCool and getcallingscript().Name == "LocalStuff" and not checkcaller() then
+			return ogWait(0.3)
+		else
+			return ogWait(x)
+		end
+	end)
+end
+
 LocalPlayerTab:CreateToggle("Remove Kill Bricks", function(value) 
     shared.RemoEKillBricks = value
-    while wait() do
-		if shared.RemoEKillBricks then
-			for i,v in pairs(game:GetService("Workspace").Map:GetChildren()) do
-				if v.Name == "Kill" then
-					v:FindFirstChild("TouchInterest"):Remove()
-				end
+	if shared.RemoEKillBricks then
+		for i,v in pairs(game:GetService("Workspace").Map:GetChildren()) do
+			if v.Name == "Kill" then
+				v:FindFirstChild("TouchInterest"):Remove()
 			end
-        end
+		end
     end
 end)
 
@@ -119,7 +137,7 @@ LocalPlayerTab:CreateToggle("Artifact Collecter", function(value)
 			LastPosition = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 			for i,v in pairs(game.Workspace:GetChildren()) do
 				if v:IsA("Tool") then
-					if v.Name == "Ice Essence" or v.Name == "Lannis Amulet" or v.Name == "Fairfrozen" or v.Name == "Lost Bulwark" or v.Name == "Spider Cloak" or v.Name == "Philo Stone" or v.Name == "Betrayer's Amulet" or v.Name == "Angel Feather" or v.Name == "Lost Bulwark" then
+					if v.Name == "Lannis Amulet" or v.Name == "Fairfrozen" or v.Name == "Lost Bulwark" or v.Name == "Spider Cloak" or v.Name == "Philo Stone" or v.Name == "Betrayer's Amulet" or v.Name == "Angel Feather" or v.Name == "Lost Bulwark" then
 						game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Handle.CFrame
 						game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = LastPosition
 					end
@@ -188,7 +206,6 @@ LocalPlayerTab:CreateToggle("Spell Adjust", function(value)
 end)
 
 
-
 LocalPlayerTab:CreateSlider("Mana Amount", 0, 100, function(arg) 
     ManaValue = arg
 end)
@@ -197,6 +214,17 @@ LocalPlayerTab:CreateSlider("Mana Climb Speed", 1, 350, function(arg)
     game.Players.LocalPlayer.Character.Stats.ClimbMult.Value = arg
 end)
 
+shared.WalkSpeedValue = 16
+
+LocalPlayerTab:CreateSlider("WalkSpeed", 16, 500, function(arg) 
+	shared.WalkSpeedValue = arg
+end)
+
+game.Players.LocalPlayer.Character.Humanoid.Changed:Connect(function()
+	if shared.WalkSpeedValue == 16 then return end
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = shared.WalkSpeedValue
+
+end)
 
 
 local GateSpots = {
@@ -217,13 +245,16 @@ for i,v in pairs(GateSpots) do
 	table.insert(Gat, i)
 end
 
+
 LocalPlayerTab:CreateDropDown("Gate Teleports", Gat, function(Talker) 
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = GateSpots[Talker]
 end)
 
 LocalPlayerTab:CreateButton("Scroll Roulette", function() 
-    fireclickdetector(game:GetService("Workspace").NPCs.Inari["Left Arm"].ClickDetector)
+	fireclickdetector(game:GetService("Workspace").NPCs.Inari["Left Arm"].ClickDetector)	  
 end)
+
+
 
 
 local VisualTab = Library:CreateTab("Visuals", "Visuals Tab")
@@ -266,23 +297,6 @@ VisualTab:CreateToggle("Max Zoom", function(value)
     end
 end)
 
-local Admins = {
-	["shadow8877sonic"] = "Mod";
-	["DrPixel"] = "Owner";
-	["lockpickk"] = "Mod";
-	["thegamerfromtheskys"] = "Mod";
-	["Drapid"] = "Mod";
-	["thegamerfromtheskys"] = "Mod";
-	["T32M"] = "Mod";
-	["theoowen"] = "Mod";
-	["Vluved"] = "Mod";
-	["Fri_ckyy"] = "Mod";
-	["Horatl0"] = "Mod";
-	["Crazecoop"] = "Mod";
-	["Seadoke18"] = "Mod";
-	["BestNameInGame"] = "Mod";
-	["stahby"] = "Mod";
-}
 
 
 
@@ -291,63 +305,114 @@ newmusic.Parent = game.Workspace
 newmusic.SoundId = "rbxassetid://1579337956"
 newmusic.Name = "RUN MODS"
 
+local Success, ErrorStatement = pcall(function()
+	local Admins = {
+		["shadow8877sonic"] = "49712669";
+		["Iockpickk"] = "249882496";
+		["thegamerfromtheskys"] = "49337985";
+		["Drapid"] = "58678007";
+		["T32M"] = "72561679";
+		["theoowen"] = "16809832";
+		["Vluved"] = "86265797";
+		["Fri_ckyy"] = "120405687";
+		["Horatl0"] = "33092329";
+		["Crazecoop"] = "56137950";
+		["Seadoke18"] = "46442436";
+		["BestNameInGame"] = "68198392";
+		["stahby"] = "322304665";
+		["Firmliest"] = "143568777";
+		["goIdiee"] = "58373448";
+	
+	}
 
-
-
-for i,v in pairs(game.Players:GetPlayers()) do
-	for I,V in pairs(Admins) do
-		if v.Name == I then
-			game.StarterGui:SetCore("SendNotification", {
-				Title = "Admin Notify";
-				Text = v.Name .. " Is in the game And Is A Mod/Admin";
-				Duration = 6
-			})
-			spawn(function()
-				newmusic:Play()
-				wait(6.5)
-				newmusic:Stop()
-			end)
+	function Callback(Text)
+		if Text == "Panic" then
+			game.Players.LocalPlayer:Kick("\n\nSafely Kicked you from the game.\n")
 		end
 	end
+	
+	local NotificationBindable = Instance.new("BindableFunction")
+	NotificationBindable.OnInvoke = Callback
+	
+	
+	
+	
+	for i,v in pairs(game.Players:GetPlayers()) do
+		for I,V in pairs(Admins) do
+			if v.Name == I or v:IsFriendsWith(V) then
+				print("ADMIN DETECTED")
+				game.StarterGui:SetCore("SendNotification", {
+					Title = "Admin Notify";
+					Text = v.Name .. " Is in the game And Is A Mod/Admin";
+					Duration = 6;
+					Button1 = "Panic";
+	
+					Callback = NotificationBindable;
+				})
+	
+				spawn(function()
+					newmusic:Play()
+					wait(6.5)
+					newmusic:Stop()
+				end)
+			end
+		end
+	end
+	
+	game.Players.PlayerAdded:Connect(function(v)
+		for I,V in pairs(Admins) do
+			if v.Name == I or v:IsFriendsWith(V) then
+				print("ADMIN DETECTED")
+				game.StarterGui:SetCore("SendNotification", {
+					Title = "Admin Notify";
+					Text = v.Name .. " Has Joined And Is A Mod/Admin";
+					Duration = 6;
+					Button1 = "Panic";
+	
+					Callback = NotificationBindable;
+				})
+				spawn(function()
+					newmusic:Play()
+					wait(6.5)
+					newmusic:Stop()
+				end)
+			end
+		end
+	end)
+	
+	game.Players.PlayerRemoving:Connect(function(v)
+		for I,V in pairs(Admins) do
+			if v.Name == I then
+				print("ADMIN DETECTED")
+				game.StarterGui:SetCore("SendNotification", {
+					Title = "Admin Notify";
+					Text = v.Name .. " Has Left And Is A Mod/Admin";
+					Duration = 6;
+					Button1 = "Panic";
+	
+					Callback = NotificationBindable;
+				})
+				spawn(function()
+					newmusic:Play()
+					wait(6.5)
+					newmusic:Stop()
+				end)
+			end
+		end
+	end)
+
+	
+end)
+
+
+if ErrorStatement then
+	game.Players.LocalPlayer:Kick("\n\nVERY IMPORTANT ERROR HAS OCCURED PLEASE DM cyzer#3829.\n")
 end
 
-game.Players.PlayerAdded:Connect(function(v)
-	for I,V in pairs(Admins) do
-		if v.Name == I then
-			game.StarterGui:SetCore("SendNotification", {
-				Title = "Admin Notify";
-				Text = v.Name .. " Has Joined And Is A Mod/Admin";
-				Duration = 6
-			})
-			spawn(function()
-				newmusic:Play()
-				wait(6.5)
-				newmusic:Stop()
-			end)
-		end
-	end
-end)
-
-game.Players.PlayerRemoving:Connect(function(v)
-	for I,V in pairs(Admins) do
-		if v.Name == I then
-			game.StarterGui:SetCore("SendNotification", {
-				Title = "Admin Notify";
-				Text = v.Name .. " Has Left And Is A Mod/Admin";
-				Duration = 6
-			})
-			spawn(function()
-				newmusic:Play()
-				wait(6.5)
-				newmusic:Stop()
-			end)
-		end
-	end
-end)
 
 game.Workspace.ChildAdded:Connect(function(v)
 	if v:IsA("Tool") then
-		if v.Name == "Ice Essence" or v.Name == "Lannis Amulet" or v.Name == "Fairfrozen" or v.Name == "Lost Bulwark" or v.Name == "Spider Cloak" or v.Name == "Philo Stone" or v.Name == "Betrayer's Amulet" or v.Name == "Angel Feather" or v.Name == "Lost Bulwark" or v.Name == "Spider Cloak" then
+		if v.Name == "Lannis Amulet" or v.Name == "Fairfrozen" or v.Name == "Lost Bulwark" or v.Name == "Spider Cloak" or v.Name == "Philo Stone" or v.Name == "Betrayer's Amulet" or v.Name == "Angel Feather" or v.Name == "Lost Bulwark" or v.Name == "Spider Cloak" then
 			game.StarterGui:SetCore("SendNotification", {
 				Title = "Artifact Notify";
 				Text = v.Name .. " Has Spawned";
@@ -356,3 +421,5 @@ game.Workspace.ChildAdded:Connect(function(v)
 		end
 	end
 end)
+
+getgenv().AlreadyExecuted = true
